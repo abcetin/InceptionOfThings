704cf7e873d88db43e1aa882614a361a852d899d
abcetin <cetin.ab@outlook.com>
2024-08-06 08:47:43 -0400

    v1

--- p2/scripts/initilaize.sh
+++ p2/scripts/initilaize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-cd "$SCRIPT_DIR"
+#SCRIPT_DIR=$(dirname "$(realpath "$0")")
+#cd "$SCRIPT_DIR"
 
-vagrant up
+#vagrant up
 
 sudo apt-get update
 sudo apt-get upgrade -y
--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+argocd=8081
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 chmod +x kubectl
@@ -17,9 +19,10 @@ sudo docker exec -it $server sh -c "echo \"nameserver 8.8.4.4\" >> /etc/resolv.c
 
 kubectl create namespace dev
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-cd ..
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl rollout status deployment/argocd-server -n argocd
+kubectl rollout status deployment/argocd-redis -n argocd
 
 kubectl apply -f confs/application.yaml
 kubectl apply -f confs/playground/deployment.yaml
@@ -35,9 +38,16 @@ kubectl create secret generic p3-secret \
     --from-literal=url=git@github.com:abcetin/InceptionOfThings.git \
     --from-file=sshPrivateKey=/home/$USERNAME/.ssh/id_rsa \
     -n argocd
+
 kubectl label secret p3-secret argocd.argoproj.io/secret-type=repo-creds -n argocd
 
 argo_passwd=$(kubectl get secret -n argocd argocd-initial-admin-secret -ojsonpath='{.data.password}' | base64 --decode)
-kubectl port-forward -n argocd svc/argocd-server 8081:443 > /dev/null 2>&1 &
-echo -e "Argocd: http://127.0.0.1:$argocd"
+#echo "kubectl port-forward -n argocd svc/argocd-server 8081:443 > /dev/null 2>&1 &"
+kubectl port-forward -n argocd --address 0.0.0.0 svc/argocd-server 8081:443 > /dev/null 2>&1 &
+#argocd login --insecure --username admin --password $argo_passwd 127.0.0.1:8081
+#if argocd repo add --insecure-skip-server-verification https://github.com/abcetin/InceptionOfThings.git; then
+#    echo "GitLab reposu başarıyla Argo CD'ye eklendi."
+#fi
+
+echo -e "Argocd: http://127.0.0.1:8081"
 echo -e "Argocd Password: $argo_passwd "
