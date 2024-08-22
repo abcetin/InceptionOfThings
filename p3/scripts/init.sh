dea15537049d74cc583b8e7b871567eaf73852ee
abcetin <cetin.ab@outlook.com>
2024-08-22 11:57:28 -0400

    .

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR"
+
 argocd=8081
+will=8080
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -24,8 +28,8 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl rollout status deployment/argocd-server -n argocd
 kubectl rollout status deployment/argocd-redis -n argocd
 
-kubectl apply -f confs/application.yaml
-kubectl apply -f confs/playground/deployment.yaml
+kubectl apply -f ../confs/application.yaml
+kubectl apply -f ../confs/deployment.yaml
 
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
@@ -35,19 +39,19 @@ USERNAME=$(whoami)
 
 kubectl create secret generic p3-secret \
     --from-literal=type=git \
-    --from-literal=url=git@github.com:abcetin/InceptionOfThings.git \
+    --from-literal=url=git@github.com:abcetin/acetin.git \
     --from-file=sshPrivateKey=/home/$USERNAME/.ssh/id_rsa \
     -n argocd
 
 kubectl label secret p3-secret argocd.argoproj.io/secret-type=repo-creds -n argocd
 
 argo_passwd=$(kubectl get secret -n argocd argocd-initial-admin-secret -ojsonpath='{.data.password}' | base64 --decode)
-#echo "kubectl port-forward -n argocd svc/argocd-server 8081:443 > /dev/null 2>&1 &"
-kubectl port-forward -n argocd --address 0.0.0.0 svc/argocd-server 8081:443 > /dev/null 2>&1 &
-#argocd login --insecure --username admin --password $argo_passwd 127.0.0.1:8081
-#if argocd repo add --insecure-skip-server-verification https://github.com/abcetin/InceptionOfThings.git; then
-#    echo "GitLab reposu başarıyla Argo CD'ye eklendi."
-#fi
 
+kubectl port-forward -n argocd --address 0.0.0.0 svc/argocd-server 8081:443 > /dev/null 2>&1 &
+kubectl port-forward -n dev --address 0.0.0.0 svc/wil-playground 8080:8888 > /dev/null 2>&1 &
+#argocd login --insecure --username admin --password $argo_passwd 127.0.0.1:8081
+
+argocd login --insecure --username admin --password $argo_passwd 0.0.0.0:8081
 echo -e "Argocd: http://127.0.0.1:8081"
+echo -e "Will: http://127.0.0.1:8080"
 echo -e "Argocd Password: $argo_passwd "
